fdf4cc2fef807d4ce1ab9a41bb7293d312aa6cac
Sky Link <s.z_link@yahoo.com>
2017-06-29 10:55:26 -0600

    Old school sum project

---
+++ src/edu/cnm/deepdive/oldschool/Sum.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive.oldschool;
+
+public class Sum {
+
+	public static void main(String[] args){
+		double sum = 0;
+		for (int i = 0; i < args.length; i++){
+			 sum += Double.parseDouble(args[i]);	
+		}
+		System.out.println(sum);
+	}
+}
